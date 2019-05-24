faf784804b64198c5c2574cecf92293949c01b2d
scy <scy@vitasoft.co.kr>
2019-05-24 11:31:36 +0900

    first

---
+++ spec/vita-simple-converter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VitaSimpleConverterView from '../lib/vita-simple-converter-view';
+
+describe('VitaSimpleConverterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
